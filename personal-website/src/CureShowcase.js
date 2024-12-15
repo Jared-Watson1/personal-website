731b5a12a9aecce79e832e74b3c1b8291a77c977
Jared-Watson1 <jared.watson@emory.edu>
2024-12-15 16:37:03 -0500

    changed cure showcase to include video demo

--- personal-website/src/CureShowcase.js
+++ personal-website/src/CureShowcase.js
@@ -1,45 +1,47 @@
 import React, { useEffect, useRef, useState } from "react";
 import {
-  ChatBubbleLeftEllipsisIcon,
-  ShieldCheckIcon,
+  ChatBubbleBottomCenterTextIcon,
+  CheckCircleIcon,
   BookOpenIcon,
-  LightBulbIcon,
-  ArrowRightOnRectangleIcon,
-  ChartBarIcon,
+  DocumentDuplicateIcon,
+  DocumentMagnifyingGlassIcon,
+  StarIcon,
+  ShieldCheckIcon,
 } from "@heroicons/react/20/solid";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-import CurePopup from "./CurePopup"; // Make sure this is the correct import path
+import CurePopup from "./CurePopup";
 
 gsap.registerPlugin(ScrollTrigger);
 
 const CureShowcase = () => {
-  const imgRefs = useRef([]);
+  const videoRef = useRef(null);
   const [isPopupOpen, setIsPopupOpen] = useState(false);
 
   useEffect(() => {
-    // GSAP animations
-    imgRefs.current.forEach((imgRef) => {
+    // GSAP animation for the video
+    if (videoRef.current) {
       gsap.fromTo(
-        imgRef,
+        videoRef.current,
         { y: -100, opacity: 0 },
         {
           y: 0,
           opacity: 1,
           scrollTrigger: {
-            trigger: imgRef,
+            trigger: videoRef.current,
             start: "top 80%",
             end: "top 20%",
             scrub: true,
           },
         }
       );
-    });
+    }
   }, []);
 
   return (
     <div className="relative isolate overflow-hidden bg-gradient-to-r from-slate-800 via-slate-900 to-slate-800 px-6 py-8 sm:py-12 lg:overflow-visible lg:px-0">
       <div className="mx-auto grid max-w-7xl grid-cols-1 gap-x-8 gap-y-16 lg:grid-cols-2 lg:gap-y-10 lg:items-center">
+        {/* Left Section: Features */}
         <div className="lg:pr-4">
           <div className="lg:max-w-lg">
             <p className="text-base font-semibold leading-7 text-indigo-600">
@@ -52,133 +54,57 @@ const CureShowcase = () => {
             </h1>
             <p className="mt-6 text-xl leading-8 text-gray-300">
               <strong>
-                Revolutionizing medical research with AI-driven solutions.
+                Streamlining scientific research with AI-driven evidence based
+                insights.
               </strong>
             </p>
             <ul className="mt-8 space-y-8 text-gray-400">
-              <li className="flex gap-x-3">
-                <ChatBubbleLeftEllipsisIcon
-                  aria-hidden="true"
-                  className="mt-1 h-5 w-5 flex-none text-indigo-600"
-                />
-                <span>
-                  <strong className="font-semibold text-gray-100">
-                    AI-Powered Chatbot:
-                  </strong>
-                  <p className="inline">
-                    {" "}
-                    Provides quick and reliable answers to clinical questions
-                    through a user-friendly interface.
-                  </p>
-                </span>
-              </li>
-              <li className="flex gap-x-3">
-                <ShieldCheckIcon
-                  aria-hidden="true"
-                  className="mt-1 h-5 w-5 flex-none text-indigo-600"
-                />
-                <span>
-                  <strong className="font-semibold text-gray-100">
-                    Patented Technology:
-                  </strong>
-                  <p className="inline">
-                    {" "}
-                    Ensures the reliability and accuracy of AI responses, backed
-                    by cutting-edge research.
-                  </p>
-                </span>
-              </li>
-              <li className="flex gap-x-3">
-                <BookOpenIcon
-                  aria-hidden="true"
-                  className="mt-1 h-5 w-5 flex-none text-indigo-600"
-                />
-                <span>
-                  <strong className="font-semibold text-gray-100">
-                    Extensive Literature Database:
-                  </strong>
-                  <p className="inline">
-                    {" "}
-                    Access over 26 million pieces of primary literature and
-                    clinical guidelines.
-                  </p>
-                </span>
-              </li>
-              <li className="flex gap-x-3">
-                <LightBulbIcon
-                  aria-hidden="true"
-                  className="mt-1 h-5 w-5 flex-none text-indigo-600"
-                />
-                <span>
-                  <strong className="font-semibold text-gray-100">
-                    Intuitive Design:
-                  </strong>
-                  <p className="inline">
-                    {" "}
-                    Transforms complex searches into simple questions, enhancing
-                    user experience.
-                  </p>
-                </span>
-              </li>
-              <li className="flex gap-x-3">
-                <ArrowRightOnRectangleIcon
-                  aria-hidden="true"
-                  className="mt-1 h-5 w-5 flex-none text-indigo-600"
-                />
-                <span>
-                  <strong className="font-semibold text-gray-100">
-                    Streamlined Retrieval:
-                  </strong>
-                  <p className="inline">
-                    {" "}
-                    Provides precise answers with quick links, previews, and
-                    citations.
-                  </p>
-                </span>
-              </li>
-              <li className="flex gap-x-3">
-                <ChartBarIcon
-                  aria-hidden="true"
-                  className="mt-1 h-5 w-5 flex-none text-indigo-600"
-                />
-                <span>
-                  <strong className="font-semibold text-gray-100">
-                    Enhanced Productivity:
-                  </strong>
-                  <p className="inline">
-                    {" "}
-                    Reduces research time, allowing healthcare professionals to
-                    focus more on patient care.
-                  </p>
-                </span>
-              </li>
+              <FeatureItem
+                icon={<ShieldCheckIcon className="h-6 w-6 text-indigo-600" />}
+                title="Patented Technology"
+                description="Cutting-edge patented technology ensures reliable, accurate AI responses for critical use cases."
+              />
+              <FeatureItem
+                icon={
+                  <ChatBubbleBottomCenterTextIcon className="h-6 w-6 text-indigo-600" />
+                }
+                title="Natural Language Queries"
+                description="Transform complex searches into simple questions. Cure AI interprets your intent for seamless query processing."
+              />
+              <FeatureItem
+                icon={<CheckCircleIcon className="h-6 w-6 text-indigo-600" />}
+                title="Verified Responses"
+                description="Rigorous literature-backed responses ensure credibility and factual accuracy for your research."
+              />
+              <FeatureItem
+                icon={<BookOpenIcon className="h-6 w-6 text-indigo-600" />}
+                title="Seamless Literature Navigation"
+                description="Access curated studies and publications with detailed information for a smooth research experience."
+              />
+              <FeatureItem
+                icon={
+                  <DocumentDuplicateIcon className="h-6 w-6 text-indigo-600" />
+                }
+                title="Quick Citation"
+                description="Easily copy or share citations, streamlining your documentation process."
+              />
+              <FeatureItem
+                icon={
+                  <DocumentMagnifyingGlassIcon className="h-6 w-6 text-indigo-600" />
+                }
+                title="Advanced Search Parameters"
+                description="Search with precision by specifying journals, dates, and more to find exactly what you need."
+              />
+              <FeatureItem
+                icon={<StarIcon className="h-6 w-6 text-indigo-600" />}
+                title="Reputable Source Prioritization"
+                description="Results are ranked by journal quality and h5-index, ensuring authoritative sources."
+              />
             </ul>
-            <p className="mt-8 text-gray-300">
-              <em>
-                Cure AI leverages LLMs to provide accurate and timely medical
-                information, improving clinical workflows and patient outcomes.
-              </em>
-            </p>
-            <h2 className="mt-16 text-2xl font-bold tracking-tight text-gray-100">
-              Simplifying Complex Medical Research
-            </h2>
-            <p className="mt-6 text-gray-300">
-              <em>
-                Cure AI transforms the way healthcare professionals conduct
-                research by providing intuitive and reliable AI-driven
-                solutions.
-              </em>
-              <br />
-              <strong>
-                Experience a streamlined process with access to extensive
-                databases and precise answers, enhancing productivity and
-                patient care.
-              </strong>
-            </p>
-            <div className="mt-10 flex space-x-3">
+            <div className="mt-10 flex space-x-4">
               <button
                 onClick={() => setIsPopupOpen(true)}
-                className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-white rounded-lg group bg-gradient-to-br from-indigo-400 to-pink-600 group-hover:from-indigo-400 group-hover:to-pink-600 hover:text-white focus:ring-4 focus:outline-none focus:ring-indigo-800"
+                className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-white rounded-lg group bg-gradient-to-br from-indigo-400 to-pink-600 group-hover:from-indigo-500 group-hover:to-pink-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-indigo-800"
               >
                 <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-gray-900 rounded-md group-hover:bg-opacity-0">
                   View Tech Stack
@@ -188,7 +114,7 @@ const CureShowcase = () => {
                 href="https://www.askcureai.com"
                 target="_blank"
                 rel="noopener noreferrer"
-                className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-white rounded-lg group bg-gradient-to-br from-indigo-400 to-pink-600 group-hover:from-indigo-400 group-hover:to-pink-600 hover:text-white focus:ring-4 focus:outline-none focus:ring-indigo-800"
+                className="relative inline-flex items-center justify-center p-0.5 mb-2 me-2 overflow-hidden text-sm font-medium text-white rounded-lg group bg-gradient-to-br from-indigo-400 to-pink-600 group-hover:from-indigo-500 group-hover:to-pink-700 hover:text-white focus:ring-4 focus:outline-none focus:ring-indigo-800"
               >
                 <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-gray-900 rounded-md group-hover:bg-opacity-0">
                   Learn More
@@ -197,45 +123,32 @@ const CureShowcase = () => {
             </div>
           </div>
         </div>
-        <div className="lg:col-start-2 lg:row-start-1 lg:flex lg:flex-col lg:items-center lg:justify-center lg:space-y-10 lg:pr-8">
-          <div
-            ref={(el) => (imgRefs.current[0] = el)}
-            className="w-full max-w-lg"
-          >
-            <img
-              src={`${process.env.PUBLIC_URL}/cure_ss_6:9:24.png`}
-              alt="Cure AI screenshot 1"
-              className="block w-full rounded-xl bg-gray-900 shadow-xl ring-1 ring-gray-400/10"
-              style={{ width: "80%", maxWidth: "40rem" }}
-            />
-          </div>
-          <div
-            ref={(el) => (imgRefs.current[1] = el)}
-            className="w-full max-w-lg"
-          >
-            <img
-              src={`${process.env.PUBLIC_URL}/cure-ss.png`}
-              alt="Cure AI screenshot 2"
-              className="block w-full rounded-xl bg-gray-900 shadow-xl ring-1 ring-gray-400/10"
-              style={{ width: "80%", maxWidth: "40rem" }}
-            />
-          </div>
-          <div
-            ref={(el) => (imgRefs.current[2] = el)}
-            className="w-full max-w-lg"
-          >
-            <img
-              src={`${process.env.PUBLIC_URL}/cure_ss_sourcepopup-7:9:24.png`}
-              alt="Cure AI screenshot 3"
-              className="block w-full rounded-xl bg-gray-900 shadow-xl ring-1 ring-gray-400/10"
-              style={{ width: "80%", maxWidth: "40rem" }}
-            />
-          </div>
+
+        {/* Right Section: Video */}
+        <div
+          className="lg:col-start-2 lg:row-start-1 flex justify-center lg:justify-end"
+          ref={videoRef}
+        >
+          <video
+            src="cure-demo-vid.mp4"
+            controls
+            className="block w-full max-w-4xl rounded-xl bg-gray-900 shadow-xl ring-1 ring-gray-400/10"
+          ></video>
         </div>
       </div>
       <CurePopup isOpen={isPopupOpen} setIsOpen={setIsPopupOpen} />
     </div>
   );
 };
+
+const FeatureItem = ({ icon, title, description }) => (
+  <li className="flex gap-x-3">
+    <div className="flex-shrink-0">{icon}</div>
+    <div>
+      <strong className="font-semibold text-gray-100">{title}</strong>
+      <p className="mt-1 text-gray-300">{description}</p>
+    </div>
+  </li>
+);
 
 export default CureShowcase;
