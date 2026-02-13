36cbf4f1392e888330839d58115e746cc94d6875
Jared-Watson1 <jared.watson@emory.edu>
2026-02-12 20:09:49 -0500

    changed vercel backend api url

--- portfolio-2026/src/lib/constants.ts
+++ portfolio-2026/src/lib/constants.ts
@@ -1,5 +1,4 @@
-export const API_BASE_URL =
-  process.env.NEXT_PUBLIC_API_URL || "http://localhost:8000";
+export const API_BASE_URL = "https://portfolio-backend-ashen-three.vercel.app/";
 
 export const LINKS = {
   github: "https://github.com/Jared-Watson1",
